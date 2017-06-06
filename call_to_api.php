2a85fa6697c8acfbf3791419766a6be4c9974cb9
javierolais <javier_olais56@hotmail.com>
2017-06-06 11:28:17 -0600

    Archivos innecesarios en el repositorio

--- call_to_api.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$url="http://localhost/api_data/api.php";
-	$ch=curl_init();
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-	curl_setopt($ch,CURLOPT_URL,$url);
-	echo curl_exec($ch);
-	curl_close($ch);
-?>
